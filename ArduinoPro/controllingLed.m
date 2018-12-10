5ef34d06ec72877e9a66cb88bff2d42202c0555a
abramReda <abramreda815@gmail.com>
2018-12-10 02:09:05 +0200

    adding out comport detection and coniction putton

--- ArduinoPro/controllingLed.m
+++ ArduinoPro/controllingLed.m
@@ -7,7 +7,7 @@ function varargout = controllingLed(varargin)
 %      the existing singleton*.
 %
 %      CONTROLLINGLED('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in CONTROLLINGLED.M with the given input arguments.
+%      function named CALLBACK in CONTROLLINGLED.popup1 with the given input arguments.
 %
 %      CONTROLLINGLED('Property','Value',...) creates a new CONTROLLINGLED or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
@@ -22,7 +22,7 @@ function varargout = controllingLed(varargin)
 
 % Edit the above text to modify the response to help controllingLed
 
-% Last Modified by GUIDE v2.5 09-Dec-2018 02:07:38
+% Last Modified by GUIDE v2.5 10-Dec-2018 01:44:33
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -60,7 +60,8 @@ guidata(hObject, handles);
 
 % UIWAIT makes controllingLed wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
-
+delete(instrfindall);
+set(handles.popup1,'String',getAvailableComPort);
 
 % --- Outputs from this function are returned to the command line.
 function varargout = controllingLed_OutputFcn(hObject, eventdata, handles) 
@@ -72,15 +73,11 @@ function varargout = controllingLed_OutputFcn(hObject, eventdata, handles)
 % Get default command line output from handles structure
 varargout{1} = handles.output;
 clear all;
+
 global x;
+
 x=serial('COM8','BAUD', 9600);
-try
-  fopen(x);
-catch
-  %delete(instrfindall);
-  fclose(instrfindall);
-  fopen(x);
-end
+
 
 
 % --- Executes on button press in pushbutton1.
@@ -99,3 +96,117 @@ function pushbutton2_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 global x;
 fprintf(x,'0');
+
+
+% --- Executes on selection change in popup1.
+function popup1_Callback(hObject, eventdata, handles)
+% hObject    handle to popup1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popup1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popup1
+
+
+% --- Executes during object creation, after setting all properties.
+function popup1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popup1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+comports=get(handles.popup1,'String');
+chosencom=get(handles.popup1,'value');
+
+global x;
+set(x,'Port',comports{chosencom});
+
+try
+  fopen(x);
+catch
+    try
+      %delete(instrfindall);
+      fclose(instrfindall);
+      fopen(x);
+    catch
+        
+    end
+end
+ 
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on key press with focus on edit1 and none of its controls.
+function edit1_KeyPressFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  structure with the following fields (see MATLAB.UI.CONTROL.UICONTROL)
+%	Key: name of the key that was pressed, in lower case
+%	Character: character interpretation of the key(s) that was pressed
+%	Modifier: name(s) of the modifier key(s) (i.e., control, shift) pressed
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes during object deletion, before destroying properties.
+function figure1_DeleteFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global x;
+fclose(x);
